b5e482ccda6fb35b8fc1428bab5292aa15ad46c2
Amojow <amaury.bloch@devinci.fr>
2018-03-03 21:50:18 +0100

    Scrap of titles, urls, and address + site and css

--- scrapMichelin.js
+++ scrapMichelin.js
@@ -3,42 +3,144 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-/*
-app.get('/scrape', function(req, res){
-    */
+
+
+//var result = 'nada';
 var i;
-for(i=1;i<3;i++){
-    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'+i;
+var listUrl = [];   
 
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
+        
+//for(i=1;i<3;i++)
+//{
 
-    request(url, function(error, response, html){
+function titleRestau(){
+    return new Promise((resolve, reject) => {
+       // url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'+i;
+        url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-2';
+        request(url, function(error, response, html){
 
         // First we'll check to make sure no errors occurred when making the request
 
         if(!error){
+
             var title;
+            var link;
             var $ = cheerio.load(html);
-            
-            var json = { title : ""};
+            var json = { title : ""}; 
+
             $('.poi_card-display-title').each(function(){
                 var data = $(this);
                 title = data.text();
+                //result.push(title);
+                result= 'skuuh';
+                //result = title;
                 console.log(title);
             });
-            json.title = title;
-            json.release = release;
-          
+            resolve(title);
+            
+            
+        
         }
-        
-        
-    })
+        });
+    });
+}
+
+
+function linkRestau(){
+    return new Promise((resolve, reject) => {
+       // url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-'+i;
+        url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-2';
+        request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+
+            var $ = cheerio.load(html);
+            
+
+
+            
+            $('.poi-card-link').each(function(){
+                var data = $(this);
+                link = data.attr('href');
+                
+                var urlTemp = "https://restaurant.michelin.fr"+link;             
+                listUrl.push(urlTemp);
+
+            });
+            
+            resolve(listUrl);
+        }
+        else{
+            console.log('error');
+        }
+        });
+    });
+}
+var j=0;
+function adressRestau(listU){
+    return new Promise((resolve, reject) => {
+        for(j=1;j<listU.length;j++){
+            url = listU[i];
+            console.log(listU[i]);
+               
+            request(url, function(error, response, html){
+
+            // First we'll check to make sure no errors occurred when making the request
+            
+            if(!error){
+
+                
+                var $ = cheerio.load(html);
+                
+
+
+                
+                var adresse;
+                var $ = cheerio.load(html);
+                $('.postal-code').each(function(){
+                    var data2 = $(this);
+                    adresse = data2.text();
+                    console.log(adresse);
+                });
+            
+            }
+            });
+        }
+    });
+    
 
 }
-app.listen('8081')
 
-console.log('Magic happens on port 8081');
 
-exports = module.exports = app;
+
+titleRestau()
+.then(linkRestau)
+.then(adressRestau)
+
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+    
+   
+    /*
+    url = url2;
+    request(url, function(error2, response2, html2){
+                    if(!error2){
+
+                        var adresse;
+                        var $ = cheerio.load(html);
+                        $('.postal-code').each(function(){
+                            var data2 = $(this);
+                            adresse = data2.text();
+                            console.log(adresse);
+                        });
+
+                    }
+
+                })
+                */
+
+
